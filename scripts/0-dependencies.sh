2a65b07f4b68ca2c6611e538714679c4f8d0d42a
Bertrand Marron <bertrand.marron@gmail.com>
2019-12-27 15:13:04 +0100

    Manually install pandoc from deb

---
+++ scripts/0-dependencies.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo '[+] Installing up-to-date version of pandoc'
+
+PANDOC_DEB=https://github.com/jgm/pandoc/releases/download/2.9.1/pandoc-2.9.1-1-amd64.deb
+
+wget $PANDOC_DEB --output-document=/tmp/pandoc.deb
+dpkg -i /tmp/pandoc.deb
+
+echo '[+] Done'
