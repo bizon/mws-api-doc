7cc85d08c038b0837701696e0541141654867de7
Bertrand Marron <bertrand.marron@gmail.com>
2020-01-06 16:17:58 +0100

    Upgrade pandoc

--- scripts/0-dependencies.sh
+++ scripts/0-dependencies.sh
@@ -2,7 +2,7 @@
 
 echo '[+] Installing up-to-date version of pandoc'
 
-PANDOC_DEB=https://github.com/jgm/pandoc/releases/download/2.9.1/pandoc-2.9.1-1-amd64.deb
+PANDOC_DEB=https://github.com/jgm/pandoc/releases/download/2.9.1.1/pandoc-2.9.1.1-1-amd64.deb
 
 wget $PANDOC_DEB --output-document=/tmp/pandoc.deb
 sudo dpkg -i /tmp/pandoc.deb
